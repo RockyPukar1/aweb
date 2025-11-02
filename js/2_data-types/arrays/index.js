f85c84db18518a15954c2e4cbcfd95c15fc529d3
Pukar Chhatkuli <dev.pukar.chhatkuli@gmail.com>
2025-11-02 20:03:47 +0545

    1

---
+++ js/2_data-types/arrays/index.js
@@ -0,0 +1,7 @@
+// Array -> collection of elements(any data type) separated comma, stored in index/position(starts from zero)
+const friends = ["Rakesh", "himesh", "Santosh"];
+                // 0       1         2
+console.log(friends)
+friends[3] = "Siddhartha"
+console.log(friends[3])
+console.log(friends)
