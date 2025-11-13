d6b4e702fe98c970d38d0f587422d7aa5b08833d
Pukar Chhatkuli <dev.pukar.chhatkuli@gmail.com>
2025-11-13 20:08:10 +0545

    react components, how frontend and backend works, started with basic faq components in react, basic css and html, loop methods in array(map), function types(arrow function)

---
+++ js/2_data-types/arrays/methods.js
@@ -0,0 +1,11 @@
+// forEach
+// map
+const data = [1, 2, 3, 4];
+const doubledData = data.map(function(d) {
+  return d * 2;
+});
+console.log(doubledData)
+// filter
+// reduce
+// some
+// each
--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from "react";
-import Button from "./components/Button";
+import "./App.css";
+
+const faqs = [
+  {
+    question: "How many bones does a cat have?",
+    answer: "A cat has 230 bones - 6 more than a human",
+  },
+  {
+    question: "How much do cats sleep?",
+    answer: "The average cat sleeps 12-16 hours per day",
+  },
+  {
+    question: "How long do cats live",
+    answer:
+      "Outdoor cats live 5 years on average. Indoor\ncats live 15 years on average.",
+  },
+];
 
 export default function App() {
-  const [count, setCount] = useState(0);
   return (
     <div>
-      <Button setCount={setCount} count={count} text={"Button 1"} />
-      <Button setCount={setCount} count={count} text={"Button 2"} />
-      <span>Number of clicks: {count}</span>
+      <h1>FAQ Section</h1>
+      {faqs.map(function (faq) {
+        return (
+          <div className="faq-block">
+            <div>{faq.question}</div>
+            <div>{faq.answer}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
